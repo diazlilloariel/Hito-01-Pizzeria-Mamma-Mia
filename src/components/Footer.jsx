7dbeb28907581cb97d750e0aa8a6a2320cf1d1a6
Ariel Diaz <diazlillo.ariel@gmail.com>
2025-06-22 19:53:47 -0400

    primer comentario

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-center text-white mt-5">
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-10">
+            <p className="text-center">
+              © 2021 - Pizzería Mamma Mia! - Todos los derechos reservados.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const Header = () => {
+  return (
+    <header className="bg-dark text-white text-center py-3">
+      <h1>Pizzería Mamma Mia</h1>
+      <p>¡Tenemos las mejores pizzas que podrás encontrar!</p>
+    </header>
+  );
+}
+export default Header;
